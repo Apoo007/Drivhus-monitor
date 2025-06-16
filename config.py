f2817e36c6326b1602d59f990b98f95964af3411
itamira <amirayoussouf234@gmail.com>
2025-06-16 17:36:42 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+THRESHOLDS = {
+    "TEMP_DAY_MIN": 15,
+    "TEMP_DAY_MAX": 30,
+    "TEMP_NIGHT_MIN": 10,
+    "TEMP_NIGHT_MAX": 25,
+    "SOIL_MIN": 20,
+    "SOIL_MAX": 80,
+    "HUM_MIN": 30,
+    "HUM_MAX": 70,
+    "LIGHT_MIN": 300
+}
+
+EMAIL = {
+    "sender": "test@example.com",
+    "password": "testpassword",
+    "receiver": "receiver@example.com"
+
+  # real_utility.py tager for denne kode
